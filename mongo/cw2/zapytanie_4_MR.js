7ad12339912a322691a061678b89348253fb5cce
Piotr Filochowski <piotr.filochowski01@gmail.com>
2021-06-04 23:42:29 +0200

    mongo cw 4 zad 4 MR

---
+++ mongo/cw2/zapytanie_4_MR.js
@@ -0,0 +1,43 @@
+
+var mapFunction = function () {
+    for (i = 0; i < this.credit.length; i++) {
+        var height = parseInt(this.height) / 100
+        var weight = parseInt(this.weight)
+        var bmi = weight / (height * height)
+        emit(this.nationality, bmi)
+    }
+}
+
+var reduceFunction = function (key, values) {
+
+    var sumBmi = 0;
+    var maxBmi = 0;
+    var minBmi = 10000;
+    for (i = 0; i < values.length; i++) {
+        sumBmi += values[i]
+        if (values[i] < minBmi) {
+            minBmi = values[i]
+        }
+        if (values[i] > maxBmi) {
+            maxBmi = values[i]
+        }
+    }
+    return {
+        nationality: key,
+        maxBmi: maxBmi,
+        minBmi: minBmi,
+        avgBmi: sumBmi / values.length
+    }
+}
+
+db.people.mapReduce(
+    mapFunction,
+    reduceFunction,
+    { out: "outCollection" }
+)
+
+
+printjson(
+    db.getCollection("outCollection").find({}).toArray()
+)
+
