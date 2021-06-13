3f31983a220b539cf2ba610f60d39a23521bb655
Piotr Filochowski <piotr.filochowski01@gmail.com>
2021-06-13 11:39:00 +0200

    fix mongo cw4 zapytanie 1 MR

--- mongo/cw2/zapytanie_1_MR.js
+++ mongo/cw2/zapytanie_1_MR.js
@@ -1,33 +1,44 @@
-
 var mapFunction = function () {
-    emit(this.sex, 
-    {
-        height: parseInt(this.height),
-        weight: parseInt(this.weight)
-    }
+    emit(
+        this.sex,
+        {
+            height: parseFloat(this.height),
+            weight: parseFloat(this.weight),
+            count: 1
+        }
     )
 }
 
 var reduceFunction = function (key, values) {
     var sumHeight = 0;
     var sumWeight = 0;
+    var sumCount = 0;
     for (i = 0; i < values.length; i++) {
         sumHeight += values[i].height
-        sumWeight += values[i].weight   
+        sumWeight += values[i].weight
+        sumCount += values[i].count
     }
     return {
-     "avgWeight": sumWeight / values.length,
-     "avgHeight": sumHeight / values.length,
-     "length": values.length
+        "height": sumHeight,
+        "weight": sumWeight,
+        "count": sumCount
+    }
+}
+
+
+var finalize2 = function (key, reducedValue) {
+    return {
+        sex: key,
+        avgHeight:  reducedValue.height / reducedValue.count,
+        avgWeight:  reducedValue.weight / reducedValue.count
     }
 }
 
 db.people.mapReduce(
     mapFunction,
     reduceFunction,
-    { out: "outCollection" }
+    {
+        out: { inline: 1 },
+        finalize: finalize2
+    }
 )
-printjson(
-db.getCollection("outCollection").find({}).toArray()
-)
-
