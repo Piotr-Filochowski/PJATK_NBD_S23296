14d8b9343fe5cb06ce1b0dcd776c37bd2e507f07
Piotr Filochowski <piotr.filochowski01@gmail.com>
2021-06-13 12:43:32 +0200

    fix zapytanie_5_mr

--- mongo/cw2/zapytanie_5_MR.js
+++ mongo/cw2/zapytanie_5_MR.js
@@ -1,34 +1,46 @@
 
 var mapFunction = function () {
     for (i = 0; i < this.credit.length; i++) {
-        emit(this.credit[i].currency, parseInt(this.credit[i].balance))
+        
+        var out = {
+            count: 1,
+            sumBalance: parseFloat(this.credit[i].balance)
+        }
+        emit(this.credit[i].currency, out)
     }
 }
 
 var reduceFunction = function (key, values) {
 
     var sumBalance = 0;
+    var sumCount= 0;
     for (i = 0; i < values.length; i++) {
-        sumBalance += values[i]
+        sumBalance += values[i].sumBalance
+        sumCount += values[i].count; 
     }
     return {
-        currency: key,
         sumBalance: sumBalance,
-        avgBmi: sumBalance / values.length
+        count: sumCount
     }
 }
 
-db.people.mapReduce(
-    mapFunction,
-    reduceFunction,
-    {
-        query: { sex: "Female", "nationality": "Poland" },
-        out: "outCollection"
+var finalize2 = function (key, reducedValue) {
+    return {
+       sumBalance: reducedValue.sumBalance,
+       avgBalance: reducedValue.sumBalance / reducedValue.count,
     }
-)
-
+}
 
 printjson(
-    db.getCollection("outCollection").find({}).toArray()
+    db.people.mapReduce(
+        mapFunction,
+        reduceFunction,
+        {
+            out: { inline: 1 },
+            query: { sex: "Female", "nationality": "Poland" },
+            finalize: finalize2
+        }
+    )
 )
 
+// 5.	Średnia i łączna ilość środków na kartach kredytowych kobiet narodowości polskiej w podziale na waluty. ,
